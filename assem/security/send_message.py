8317bcbe0f0c72d6585417f6e01a9814f0875708
newri <bagzhankarl@gmail.com>
2024-09-27 15:30:41 +0600

    last update

--- assem/security/send_message.py
+++ assem/security/send_message.py
@@ -19,8 +19,3 @@ def send_message(token, typing_time, to, body):
     response = requests.post(url, json=payload, headers=headers)
     return response.text
 
-
-# Example usage
-token = "rmXQbivTFcJHwIpQtDWu58co0TaeqOCN"
-response_text = send_message(token, 0, "77761174378", "Привет")
-print(response_text)
