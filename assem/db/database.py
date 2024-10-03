0482c38f8542c0db7264a4834909380f0074f003
newri <bagzhankarl@gmail.com>
2024-10-03 09:58:47 +0600

    last update

--- assem/db/database.py
+++ assem/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/assem"  # Подключение к MariaDB
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://assemuser:assemuser@localhost/assemcrm"  # Подключение к MariaDB
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
